461fb8c7198c0c32dd70779bc514fad77b731ab2
varsha <01fe22bca019@kletech.ac.in>
2024-03-10 21:00:09 +0530

    code for student information

---
+++ Student_info.java
@@ -0,0 +1,22 @@
+public class Student_info {
+    String name = "varsha";
+    String department = "BCA";
+    String program = "BCA";
+    public String RegistertedCourse[] = { " ", "C#", "Applied statistics", "Java" };
+
+    public void display() {
+        System.out.println("----Student Details----");
+        System.out.println("Name:" + name);
+        System.out.println("Department: " + department);
+        System.out.println("Program:" + program);
+        System.out.print("Registered Courses:\n");
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + ":" + RegistertedCourse[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Student_info s = new Student_info();
+        s.display();
+    }
+}
